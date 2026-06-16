6475ca42d9fdd806692fce1d562c33ff7603f21c
HyeonokKim <hyok9001@gmail.com>
2026-06-16 14:54:00 +0900

    fix(db): 주간 요약 저장 마이그레이션 ID 수정
    Alembic revision 길이 제한 대응
    
    weekly_summary_exports 마이그레이션 적용 실패 수정

--- backend/alembic/versions/0007_create_weekly_summary_exports.py
+++ backend/alembic/versions/0007_create_weekly_summary_exports.py
@@ -1,6 +1,6 @@
 """create weekly summary exports
 
-Revision ID: 0007_create_weekly_summary_exports
+Revision ID: 0007_weekly_summary_exports
 Revises: 0006_add_attempt_solve_duration
 Create Date: 2026-06-16
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 
-revision: str = "0007_create_weekly_summary_exports"
+revision: str = "0007_weekly_summary_exports"
 down_revision: Union[str, None] = "0006_add_attempt_solve_duration"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
